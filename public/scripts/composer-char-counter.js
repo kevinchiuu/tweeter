d52a9265b7cecb3e7afde999fc5c9d795cb034e6
kevin chiu <kevinchiu@kevins-MacBook-Air.local>
2021-05-19 20:53:02 -0400

    remove whitespace and rename variables

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,19 @@
+/* eslint-disable no-undef */
+/* eslint-env jquery */
 $(document).ready(function() {
 
   $("#tweet-text").on('keyup', function() {
     const max = 140;
     const charCount = $(this).val().length;
-    const remaining = max - charCount;
+    const charRemaining = max - charCount;
     const $counter = $(this).parent().find('.counter');
 
-    $counter.text((remaining));
+    $counter.text(charRemaining);
 
-    if (remaining < 0) {
+    if (charRemaining < 0) {
       $counter.addClass('error');
 
-    } else if (remaining >= 0) {
+    } else if (charRemaining >= 0) {
       $counter.removeClass('error');
     }
   });
